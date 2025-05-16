ac4ec715e871f8c586b3ac288a64c7bd942d192b
jenri019 <j.enrique209436@hotmail.com>
2025-05-16 16:54:33 -0600

    feat: adding component & routes to store-front

---
+++ src/app/store-front/components/navbar/navbar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'store-front-navbar',
+  imports: [],
+  templateUrl: './navbar.component.html',
+})
+export class NavbarComponent { }
