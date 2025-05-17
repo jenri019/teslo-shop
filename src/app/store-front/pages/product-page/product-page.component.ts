5418c97b225602432684be66efb903c88ff309ce
jenri019 <j.enrique209436@hotmail.com>
2025-05-17 16:29:23 -0600

    feat: adding image carousel using swiper

--- src/app/store-front/pages/product-page/product-page.component.ts
+++ src/app/store-front/pages/product-page/product-page.component.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { ProductsService } from '@products/services/products.service';
+import { ProductCarouselComponent } from "../../../products/components/product-carousel/product-carousel.component";
 
 @Component({
     selector: 'app-product-page',
-    imports: [],
+    imports: [ProductCarouselComponent],
     templateUrl: './product-page.component.html',
 })
 export default class ProductPageComponent {
