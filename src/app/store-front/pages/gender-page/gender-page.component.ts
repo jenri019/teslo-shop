3ad108aa61edae1c31eec3752bacc88d7d5ce2c2
jenri019 <j.enrique209436@hotmail.com>
2025-05-17 16:41:22 -0600

    feat: getting products by gender

--- src/app/store-front/pages/gender-page/gender-page.component.ts
+++ src/app/store-front/pages/gender-page/gender-page.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '@products/services/products.service';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
+import { ProductCardComponent } from "../../../products/components/product-card/product-card.component";
 
 @Component({
-  selector: 'app-gender-page',
-  imports: [],
-  templateUrl: './gender-page.component.html',
+    selector: 'app-gender-page',
+    imports: [ProductCardComponent],
+    templateUrl: './gender-page.component.html',
 })
-export default class GenderPageComponent { }
+export default class GenderPageComponent {
+    route = inject(ActivatedRoute);
+    _productsService = inject(ProductsService);
+
+    gender = toSignal(
+        this.route.params.pipe(
+            map(({ gender }) => gender)
+        )
+    )
+
+    productsResource = rxResource({
+        request: () => ({ gender: this.gender() }),
+        loader: ({ request }) => {
+            return this._productsService.getProducts({
+                gender: request.gender
+            });
+        }
+    })
+}
